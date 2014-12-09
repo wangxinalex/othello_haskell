eeaba95a2723dc675a1d25d2382c4b824bd16a20
wangxinalex <wangxinalex@gmail.com>
2014-12-09 21:38:29 +0800

    put pieces on board

--- GUI.hs
+++ GUI.hs
@@ -6,6 +6,13 @@ import Data.Maybe
 levels :: [(String, Int)]
 levels = [("Easy", 2), ("Medium", 4), ("Hard", 8)]
 
+boardIndex = 0
+blackIndex = 1
+whiteIndex = 2
+
+boardWidth :: Int
+boardWidth = 8
+
 defaultLevel :: Int
 defaultLevel = 2
 
@@ -14,6 +21,9 @@ padding = 27
 
 width :: Int
 width = 56
+
+type Board = [Position]
+data Position = Black | White | Empty deriving Eq
 
 getPosition::(Int, Int)->(Int,Int)
 getPosition (x,y) = (x_pos, y_pos) 
@@ -27,6 +37,8 @@ gui = do
         f <- frame [text := "Othello" , picture := "./img/icon.png"]
         
         boardBmp <- bitmapCreateLoad "./img/board.png" wxBITMAP_TYPE_PNG
+        whitePieceBmp <- bitmapCreateLoad "./img/white.png" wxBITMAP_TYPE_PNG
+        blackPieceBmp <- bitmapCreateLoad "./img/black.png" wxBITMAP_TYPE_PNG
 
         boardPanel <- panel f [clientSize := sz 500 500 ] 
         
@@ -57,7 +69,7 @@ gui = do
         about <- menuAbout hlpMenu [help := "About the program"]
 
         set boardPanel [on resize := repaint boardPanel,
-                        on paint  := drawBackground boardBmp,
+                        on paint  := drawBackground [boardBmp, blackPieceBmp, whitePieceBmp] [White, Empty, Black,Black,Black,Black,White,Empty,White,Black,Empty,Empty, Black, White, Black, White, Black, Empty, White],
                         on click  := putPieces boardPanel]
 
         set f [statusBar := [status],
@@ -71,7 +83,30 @@ putPieces pan (Point x y)
          return ()
         
 
-drawBackground :: Bitmap() -> DC() -> Rect -> IO()
-drawBackground bmp dc (Rect x y w h) = 
-        do drawBitmap dc bmp pointZero False []
+drawBackground :: [Bitmap()] -> Board -> DC() -> Rect -> IO()
+drawBackground bmps pieces dc (Rect x y w h) = 
+        do drawBitmap dc (bmps !! boardIndex) pointZero False []
+           drawPieces dc pieces bmps
            return ()
+
+drawPieces :: DC() -> Board ->[Bitmap()] -> IO ()
+drawPieces dc gameBoard bmps = 
+    do 
+        for 0 ((length gameBoard) - 1) (\i ->
+               case gameBoard !! i of
+                    Black -> drawBitmap dc (bmps !! blackIndex) (generatePosition i) False []
+                    White -> drawBitmap dc (bmps !! whiteIndex) (generatePosition i) False []
+                    _     -> putStrLn "Empty" )
+        return ()
+
+for :: Int -> Int -> (Int -> IO ()) -> IO ()
+for x y f = sequence_ $ map f [x..y]
+
+generatePosition :: Int -> Point
+generatePosition index  
+            | index < 0 || index >= boardWidth*boardWidth = error "Invalid position"
+            | otherwise = pt x y 
+                            where x = (index `mod` boardWidth)*width+padding
+                                  y = (index `div` boardWidth)*width+padding
+       
+
