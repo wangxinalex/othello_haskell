c6d1af0b56905d4033df20c76cb6dfe707a7e0ea
wangxinalex <wangxinalex@gmail.com>
2014-12-09 16:44:58 +0800

    1. replace all tabs with spaces  2. click the board to see the position

--- GUI.hs
+++ GUI.hs
@@ -9,54 +9,69 @@ levels = [("Easy", 2), ("Medium", 4), ("Hard", 8)]
 defaultLevel :: Int
 defaultLevel = 2
 
+padding :: Int
+padding = 27
+
+width :: Int
+width = 56
+
+getPosition::(Int, Int)->(Int,Int)
+getPosition (x,y) = (x_pos, y_pos) 
+    where x_pos = ((x - padding) `div` width) 
+          y_pos = ((y - padding) `div` width)
+
 main = start gui
 
 gui :: IO ()
 gui = do
-		f <- frame [text := "Othello" , picture := "./img/icon.png"]
-		
-		boardBmp <- bitmapCreateLoad "./img/board.png" wxBITMAP_TYPE_PNG
+        f <- frame [text := "Othello" , picture := "./img/icon.png"]
+        
+        boardBmp <- bitmapCreateLoad "./img/board.png" wxBITMAP_TYPE_PNG
 
-		boardPanel <- panel f [clientSize := sz 500 500 ] 
-		
-		status <- statusField [text := "Welcome to Othello"]
-		gameMenu <- menuPane [text := "&Game"]
-		new <- menuItem gameMenu [text := "New",
-								  help := "Restart the game"]
-		menuLine gameMenu
-		menuQuit gameMenu [help := "Quit the game",
-						   on command := close f]
+        boardPanel <- panel f [clientSize := sz 500 500 ] 
+        
+        status <- statusField [text := "Welcome to Othello"]
+        gameMenu <- menuPane [text := "&Game"]
+        new <- menuItem gameMenu [text := "New",
+                                  help := "Restart the game"]
+        menuLine gameMenu
+        menuQuit gameMenu [help := "Quit the game",
+                           on command := close f]
 
-		optMenu <- menuPane [text := "Options"]
-		r0 <- menuItem optMenu [text := "Human first",
-								help := "Choose starting player",
-								checkable := True, checked := True]
-		menuLine optMenu
-		rs <- sequence [menuRadioItem optMenu 
-						[text := txt, 
-						 help := "Choose the opponent level"]
-						|(txt, l) <- levels]
-		sequence_ [set r [checked := True]
-					| (r,l) <- zip rs (map snd levels), 
-						l == defaultLevel]
+        optMenu <- menuPane [text := "Options"]
+        r0 <- menuItem optMenu [text := "Human first",
+                                help := "Choose starting player",
+                                checkable := True, checked := True]
+        menuLine optMenu
+        rs <- sequence [menuRadioItem optMenu 
+                        [text := txt, 
+                         help := "Choose the opponent level"]
+                        |(txt, l) <- levels]
+        sequence_ [set r [checked := True]
+                    | (r,l) <- zip rs (map snd levels), 
+                        l == defaultLevel]
 
-		hlpMenu <- menuHelp []
-		rules <- menuItem hlpMenu [text := "Rules",
-								   help := "About the game rules"]
-		about <- menuAbout hlpMenu [help := "About the program"]
+        hlpMenu <- menuHelp []
+        rules <- menuItem hlpMenu [text := "Rules",
+                                   help := "About the game rules"]
+        about <- menuAbout hlpMenu [help := "About the program"]
 
-		set boardPanel [on resize := repaint boardPanel,
-						on paint  := drawBackground boardBmp]
+        set boardPanel [on resize := repaint boardPanel,
+                        on paint  := drawBackground boardBmp,
+                        on click  := putPieces boardPanel]
 
-		set f [statusBar := [status],
-			   menuBar   := [gameMenu, optMenu, hlpMenu],
-			   layout    :=   minsize (sz 500 500) $ widget boardPanel]
+        set f [statusBar := [status],
+               menuBar   := [gameMenu, optMenu, hlpMenu],
+               layout    :=   minsize (sz 500 500) $ widget boardPanel]
+
+putPieces :: Panel() -> Point -> IO ()
+putPieces pan (Point x y)
+    = do let (x_pos, y_pos) = getPosition (x,y)
+         putStrLn $ "x = " ++ show x_pos ++ ", y = "++ show y_pos
+         return ()
+        
 
 drawBackground :: Bitmap() -> DC() -> Rect -> IO()
 drawBackground bmp dc (Rect x y w h) = 
-		do 
-			drawBitmap dc bmp pointZero False []
-			return ()
-
-
-
+        do drawBitmap dc bmp pointZero False []
+           return ()
