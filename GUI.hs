1ccfbf8eb7f73510c346fce992778871f7c596f3
wangxinalex <wangxinalex@gmail.com>
2014-12-19 14:51:38 +0800

    add some menu function

--- GUI.hs
+++ GUI.hs
@@ -2,6 +2,7 @@
 import Graphics.UI.WX
 import Graphics.UI.WXCore
 import Data.Maybe
+{-import BW-}
 
 levels :: [(String, Int)]
 levels = [("Easy", 2), ("Medium", 4), ("Hard", 8)]
@@ -9,9 +10,6 @@ levels = [("Easy", 2), ("Medium", 4), ("Hard", 8)]
 boardIndex = 0
 blackIndex = 1
 whiteIndex = 2
-
-boardWidth :: Int
-boardWidth = 8
 
 defaultLevel :: Int
 defaultLevel = 2
@@ -22,12 +20,17 @@ padding = 27
 width :: Int
 width = 56
 
-type Board = [Position]
-data Position = Black | White | Empty deriving Eq
-type Step = (Int, Int, Position)
+boardWidth :: Int
+boardWidth = 8
+
+type Board = [Piece]
+data Piece = Black | White | Empty deriving Eq
+type Position = (Int, Int)
+type Step = (Position, Piece) -- ((Int, Int), Piece)
+type Simple_Board = [Step] -- [((Int, Int), Piece)]
 
 newBoard :: Int -> Board
-newBoard width = replicate (width*width) Empty
+newBoard width = (replicate pad Empty) ++ [White, Black] ++ (replicate (width - 2) Empty) ++ [Black, White] ++ (replicate pad Empty) where pad = (width+1)*((width `div` 2) - 1)
 
 main = start gui
 
@@ -66,8 +69,11 @@ gui = do
 
         hlpMenu <- menuHelp []
         rules <- menuItem hlpMenu [text := "Rules",
-                                   help := "About the game rules"]
-        about <- menuAbout hlpMenu [help := "About the program"]
+                                   help := "About the game rules",
+                                   on command := infoRules f]
+
+        about <- menuAbout hlpMenu [help := "About the program",
+                                    on command := infoAbout f]
 
         set boardPanel [on resize := repaint boardPanel,
                         on paint  := drawBackground [boardBmp, blackPieceBmp, whitePieceBmp] gameBoard,
@@ -77,11 +83,36 @@ gui = do
                menuBar   := [gameMenu, optMenu, hlpMenu],
                layout    :=  minsize (sz 500 500) $ widget boardPanel]
 
-putPieces :: Panel() -> Var Board -> Var Position -> Point -> IO ()
+        set new [on command := do varUpdate gameBoard reinitializeBoard
+                                  repaint boardPanel]
+
+reinitializeBoard :: Board -> Board
+reinitializeBoard b = newBoard boardWidth
+
+infoAbout :: Frame a -> IO()
+infoAbout w
+     = infoDialog w "About Othello" $
+       init $
+       unlines ["Written in Haskell using the wxWidgets toolkit",
+                "by wangxinalex <wangxinalex@gmail.com>",
+                "and zccshome <zijinhuakaile@sina.com>",
+                "Published by http://io.wxa.me"
+                ]
+
+infoRules :: Frame a -> IO()
+infoRules w
+     = infoDialog w "Rules of Othello" $
+       init $
+       unlines ["Player and Computer take turns putting pieces on the board.",
+                "All consecutive pieces between two opposite pieces will be reversed.",
+                "The first player who cannot put valid pieces loses the game."
+                ]
+
+putPieces :: Panel() -> Var Board -> Var Piece -> Point -> IO ()
 putPieces pan varBoard varColor (Point x y) = 
     do color <- varGet varColor
-       let (x_pos, y_pos) = getPosition (x,y)
-           step = (x_pos, y_pos, color)
+       let (x_pos, y_pos) = getPiece (x,y)
+           step = ((x_pos, y_pos), color)
        board <- varGet varBoard
        putStrLn $ "x = " ++ show x_pos ++ ", y = "++ show y_pos
        varUpdate varBoard (changeBoard step)
@@ -93,10 +124,10 @@ putPieces pan varBoard varColor (Point x y) =
  1. within the range of board
  2. no picec in the current position-}
 validStep :: Board -> Step -> Bool
-validStep board (x,y,position) = x >= 0 && x < boardWidth && y >= 0 && y < boardWidth && (board !! (positionToIndex x y) == Empty)
+validStep board ((x,y),position) = x >= 0 && x < boardWidth && y >= 0 && y < boardWidth && (board !! (positionToIndex x y) == Empty)
 
 {-change the piece color for a valid step-}
-changeColor :: Board -> Step -> Position -> Position
+changeColor :: Board -> Step -> Piece -> Piece
 changeColor board step color
     | not (validStep board step) = color       
     | color == Black  = White
@@ -104,8 +135,8 @@ changeColor board step color
 
 {-change the board status for a valid step-}
 changeBoard :: Step -> Board -> Board
-changeBoard (x,y,position) board 
-    | not (validStep board (x,y,position)) = board
+changeBoard ((x,y),position) board 
+    | not (validStep board ((x,y),position)) = board
     | otherwise = (take index board) ++ position : (drop (index + 1) board) where index = positionToIndex x y
 
 positionToIndex:: Int -> Int -> Int
@@ -122,22 +153,22 @@ drawPieces :: DC() -> Board ->[Bitmap()] -> IO ()
 drawPieces dc gameBoard bmps = 
     do  for 0 ((length gameBoard) - 1) (\i ->
                case gameBoard !! i of
-                    Black -> drawBitmap dc (bmps !! blackIndex) (generatePosition i) False []
-                    White -> drawBitmap dc (bmps !! whiteIndex) (generatePosition i) False []
+                    Black -> drawBitmap dc (bmps !! blackIndex) (generatePiece i) False []
+                    White -> drawBitmap dc (bmps !! whiteIndex) (generatePiece i) False []
                     _     -> return () )
         return ()
 
 for :: Int -> Int -> (Int -> IO ()) -> IO ()
 for x y f = sequence_ $ map f [x..y]
 
-getPosition::(Int, Int)->(Int,Int)
-getPosition (x,y) = 
+getPiece::(Int, Int)->(Int,Int)
+getPiece (x,y) = 
     let x_pos = ((x - padding) `div` width) 
         y_pos = ((y - padding) `div` width)
     in (x_pos, y_pos) 
 
-generatePosition :: Int -> Point
-generatePosition index  
+generatePiece :: Int -> Point
+generatePiece index  
     | index < 0 || index >= boardWidth*boardWidth = error "Invalid position"
     | otherwise = pt x y 
                   where x = (index `mod` boardWidth)*width+padding
